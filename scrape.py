a0bb7703020a75087cedf4d26cc1cceb8a8d861f
Paul <paul@Pauls-MacBook-Pro.local>
2025-06-02 21:53:52 +0200

    BW icon

--- scrape.py
+++ scrape.py
@@ -1341,7 +1341,7 @@ HTML_TMPL = """<!doctype html>
   <!-- quick-filter chips -->
   <div class="filter-bar">
         <img
-        src="logos/apple-touch-icon.png"
+        src="logos/apple-touch-icon-bw.png"
         alt="Pathé"
         class="chip-logo"
         id="refresh"
